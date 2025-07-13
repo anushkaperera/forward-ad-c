5743f0563c0cf27eddc1dc763cc78dc6bf915904
Anushka Perera <anushka.perera@yara.com>
2025-07-14 00:29:35 +0200

    modified:   tests/test_forward-ad-c.c

--- tests/test_forward-ad-c.c
+++ tests/test_forward-ad-c.c
@@ -7,6 +7,7 @@ int main() {
     DualVar *a = dvar_create(2);
     DualVar *b = dvar_create(2);
     DualVar *r = dvar_create(2);
+    DualVar *r2 = dvar_create(2);
     a->value = 1.0;
     a->grad[0] = 1.0;
     a->grad[1] = 0.0;
@@ -14,7 +15,8 @@ int main() {
     b->grad[1] = 1.0;
     b->value = 2.0;
     r = dvar_add(a, b);
-    printf("%f %f %f\n", r->value, r->grad[0], r->grad[1]);
+    r2 = dvar_add(r, b);
+    printf("%f %f %f\n", r2->value, r2->grad[0], r2->grad[1]);
     if (a) {
         dvar_free(a);
     }
